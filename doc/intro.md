4bc2c005c6fb9d9c34bf0dd6db98e84ad41730ff
feliperibeiro <felipe.ribeirosp@gmail.com>
2019-01-20 23:05:14 -0200

    Projeto Alunos

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to processing-with-clojure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/processing_with_clojure/core.clj
@@ -0,0 +1,37 @@
+(ns processing-with-clojure.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
+
+(+ 5 4)
+(defn soma [x y] (+ x y))
+
+(defn nome [] (println "Felipe Ribeiro"))
+
+(def aluno1 {:name "Felipe" :age 27 :materia "matematica" :nota 10})
+(def aluno2 {:name "Maria" :age 24 :materia "portugues" :nota 8})
+(def aluno3 {:name "Joao" :age 18 :materia "matematica" :nota 4})
+
+(def listaAlunos (list aluno1 aluno2 aluno3))
+
+(def listaNomes (list "Felipe" "Mariana" "Carol" "Pablo" "Carlos"))
+
+(def listaMaterias (list "Portugues" "Matematica" "Biologia" "Historia"))
+
+(defn filtraMelhoresAlunos [lista] ( filter #(> (:nota %) 9) lista ))
+
+(defn aluno []
+  (let [
+        nome (rand-nth listaNomes)
+        idade (+ (rand-int 15) 10)
+        nota (rand-int 10)
+        materia (rand-nth listaMaterias)]
+    {:nome nome :idade idade  :materia materia :nota nota}))
+
+(defn novaListaAlunos [quantidade]
+      ( take quantidade (repeatedly #( conj []  (aluno))  )))
+
+
